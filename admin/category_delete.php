8110c41680f44cdbd963d91ae7edb558d2e6f9a7
ThawHtooZin <thawhtooin@gmail.com>
2026-02-09 16:19:01 +0630

    category delete fix

--- admin/category_delete.php
+++ admin/category_delete.php
@@ -1,6 +1,11 @@
 <?php
-include 'Config/config.php';
-$stmt = $pdo->prepare("DELETE FROM categories WHERE id=".$_GET['id']);
-$stmt->execute();
+include '../config/config.php';
+
+if (isset($_GET['id'])) {
+    $stmt = $pdo->prepare("DELETE FROM categories WHERE id = :id");
+    $stmt->bindValue(':id', (int)$_GET['id'], PDO::PARAM_INT);
+    $stmt->execute();
+}
+
 header('Location: category.php');
- ?>
+?>
